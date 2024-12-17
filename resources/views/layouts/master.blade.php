d47d641d9b50ac4de41b0279ca02f5aa43012dd7
LuciferLovesMe <abhiemsmile@gmail.com>
2024-12-18 02:56:07 +0700

    develop: fix layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -96,18 +96,35 @@
     <script src="{{ asset('js/stisla.js') }}"></script> --}}
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <!-- Bootstrap Bundle -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <!-- DataTables -->
+    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script> --}}
     <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
     <!-- Template JS -->
     <script src="{{ asset('assets/js/main.js') }}"></script>
-    <script src="{{ asset('js/custom.js') }}"></script>
+    {{-- <script src="{{ asset('js/custom.js') }}"></script> --}}
 
     @stack('scripts')
     <!-- Template JS File -->
-    <script src="{{ asset('js/scripts.js') }}"></script>
-    <script src="{{ asset('js/custom.js') }}"></script>
+    {{-- <script src="{{ asset('js/scripts.js') }}"></script>
+    <script src="{{ asset('js/custom.js') }}"></script> --}}
+
+    {{-- script accordion --}}
+
+    <script>
+        $(".accordion-button").on('click', function(e) {
+            console.log($("#collapseTwo").attr('class'));
+            
+            if($(".accordion-collapse").hasClass('show')) {
+                console.log('test');
+            } else {
+                console.log('123');
+                
+            }
+        })
+    </script>
 </body>
 
 </html>
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -18,6 +18,18 @@
     @php
         $level = Auth::user()->role;
     @endphp
+    {{-- <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+            </button>
+          </h2>
+          <div id="collapseTwo" class="accordion-collapse collapse item-sidebar" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+            </div>
+        </div>
+        </div>
+    </div> --}}
     <ul class="menu-inner py-1">
         <!-- Dashboards -->
         <li class="menu-item">
